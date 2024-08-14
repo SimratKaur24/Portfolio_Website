cb25cad1cf272fec58c07726c875d433a4430c48
Simrat Kaur <128754409+SimratKaur24@users.noreply.github.com>
2024-08-14 16:50:30 -0700

    Update navbar.js

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './navbar.css';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.jpeg';
 import contactImg from '../../assets/contact.png';
 import { Link } from 'react-scroll';
 import menu from '../../assets/menu.png';
