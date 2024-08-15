d72cebaf8b12c2025b0fa1af09e089f8d194318e
Simrat Kaur <128754409+SimratKaur24@users.noreply.github.com>
2024-08-14 22:18:40 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/NavBar/navbar";
 import Intro from './components/Intro/intro';
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
-import Contact from "./components/Contact/contact";
-import Footer from "./components/Footer/footer";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
+import HireMe from './components/HireMe'; 
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <Intro />
-            <Skills />
-            <Works />
-            <Contact />
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={
+                        <>
+                            <Intro />
+                            <Skills />
+                            <Works />
+                            <Contact />
+                            <Footer />
+                        </>
+                    } />
+                    <Route path="/hire-me" element={<HireMe />} /> 
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
 export default App;
-
